fb6a9cd36e841c0030160afbdfa37e6ed930580d
kaitlinhull <kaitlinhull6@gmail.com>
2025-02-27 16:53:23 -0500

    Update R07_ERR09_J.java

--- R07_ERR09_J.java
+++ R07_ERR09_J.java
@@ -1,7 +1,48 @@
+class PasswordSecurityManager extends SecurityManager {
+  private boolean isExitAllowedFlag;
+   
+  public PasswordSecurityManager(){
+    super();
+    isExitAllowedFlag = false; 
+  }
+  
+  public boolean isExitAllowed(){
+    return isExitAllowedFlag;   
+  }
+  
+  @Override
+  public void checkExit(int status) {
+    if (!isExitAllowed()) {
+      throw new SecurityException();
+    }
+    super.checkExit(status);
+  }
+  
+  public void setExitAllowed(boolean f) {
+    isExitAllowedFlag = f;  
+  }
+}
+ 
 public class InterceptExit {
   public static void main(String[] args) {
+    PasswordSecurityManager secManager =
+        new PasswordSecurityManager();
+    System.setSecurityManager(secManager);
+    try {
+      // ...
+      System.exit(1);  // Abrupt exit call
+    } catch (Throwable x) {
+      if (x instanceof SecurityException) {
+        System.out.println("Intercepted System.exit()");
+        // Log exception
+      } else {
+        // Forward to exception handler
+      }
+    }
+ 
     // ...
-    System.exit(1);  // Abrupt exit
-    System.out.println("This never executes");
+    secManager.setExitAllowed(true);  // Permit exit
+    // System.exit() will work subsequently
+    // ...
   }
 }
